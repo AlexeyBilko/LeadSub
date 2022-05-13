ba05b2ce0454eca0f638ef7ef76587368a61d3a9
Alexey Bilko <bilkoalexey67@gmail.com>
2022-05-13 14:25:43 +0300

    Добавьте файлы проекта.

---
+++ LeadSub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeadSub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
