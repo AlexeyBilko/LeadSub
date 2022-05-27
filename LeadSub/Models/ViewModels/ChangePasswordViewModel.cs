c9e3879ec5070b4f04ff2329a16809ee8c2ebc3a
Alexey Bilko <bilkoalexey67@gmail.com>
2022-05-27 16:09:33 +0300

    added account controller and related viewmodels

---
+++ LeadSub/Models/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeadSub.Models.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+        public string UserId { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string OldPassword { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string ConfirmNewPassword { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
