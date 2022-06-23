4f7a774ec3fbc26f47413bd96e9ca52b6a31b3ad
Alexey Bilko <bilkoalexey67@gmail.com>
2022-06-23 15:12:15 +0300

    Added Api Account Controller

---
+++ LeadSub/APIConfig/AuthOptions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeadSub.APIConfig
+{
+    public class AuthOptions
+    {
+        public const string ISSUER = "LeadSubUser";
+        public const string AUDIENCE = "LeadSub";
+        const string KEY = "APISECRETSECURITYKEY";
+        public const int LIFETIME = 20;
+        public static SymmetricSecurityKey GetSymetricKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+        }
+
+    }
+}
