1b30fbcf2478a2f0e5c8ca69c12857970a062ea9
Nick <mittsykh@ukr.net>
2022-06-25 17:08:56 +0300

    Create MySubPages page

---
+++ LeadSub/Controllers/SubPagesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LeadSub.Controllers
+{
+    public class SubPagesController : Controller
+    {
+        public IActionResult MySubPages()
+        {
+            return View();
+        }
+    }
+}
